d8c2ba5be31595dcbd79cd42c6a2fa30b40b7da1
Gavin Rapp <112211271+GavinRapp@users.noreply.github.com>
2022-10-16 21:05:39 -0600

    Add files via upload

---
+++ HidingSecretKeyWwindowsVar.py
@@ -0,0 +1,7 @@
+#hiding secret key in password / windows environment variable
+import os
+email_id = os.environ['my_email']
+my_pass = os.environ['my_password]
+
+print ("My email id is ", email_id)
+print ("Password for: {} is | {}".format)
